6cc6f423f5a45354a435229da561231abb9c9428
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-03-19 20:27:47 +0530

    data_dwlr.js

---
+++ model/data_dwlr.js
@@ -0,0 +1,77 @@
+const case3Data = [
+  {
+    "STATE": "Andaman and Nicobar",
+    "NUMBER_OF_WELLS_ANALYSED": 99,
+    "RISE": {
+      "0-2m": { "COUNT": 70, "PERCENTAGE": 70.7 },
+      "2-4m": { "COUNT": 2, "PERCENTAGE": 2 },
+      "4m+": { "COUNT": 0, "PERCENTAGE": 0 }
+    },
+    "FALL": {
+      "0-2m": { "COUNT": 26, "PERCENTAGE": 26.3 },
+      "2-4m": { "COUNT": 0, "PERCENTAGE": 0 },
+      "4m+": { "COUNT": 0, "PERCENTAGE": 0 }
+    },
+    "NO_CHANGE": { "COUNT": 1, "PERCENTAGE": 1 }
+  },
+  {
+    "STATE": "Andhra Pradesh",
+    "NUMBER_OF_WELLS_ANALYSED": 662,
+    "RISE": {
+      "0-2m": { "COUNT": 335, "PERCENTAGE": 50.6 },
+      "2-4m": { "COUNT": 91, "PERCENTAGE": 13.7 },
+      "4m+": { "COUNT": 65, "PERCENTAGE": 9.8 }
+    },
+    "FALL": {
+      "0-2m": { "COUNT": 157, "PERCENTAGE": 23.7 },
+      "2-4m": { "COUNT": 11, "PERCENTAGE": 1.7 },
+      "4m+": { "COUNT": 3, "PERCENTAGE": 0.5 }
+    },
+    "NO_CHANGE": { "COUNT": 0, "PERCENTAGE": 0 }
+  },
+  {
+    "STATE": "Arunachal Pradesh",
+    "NUMBER_OF_WELLS_ANALYSED": 9,
+    "RISE": {
+      "0-2m": { "COUNT": 1, "PERCENTAGE": 11.1 },
+      "2-4m": { "COUNT": 0, "PERCENTAGE": 0 },
+      "4m+": { "COUNT": 0, "PERCENTAGE": 0 }
+    },
+    "FALL": {
+      "0-2m": { "COUNT": 7, "PERCENTAGE": 77.8 },
+      "2-4m": { "COUNT": 1, "PERCENTAGE": 11.1 },
+      "4m+": { "COUNT": 0, "PERCENTAGE": 0 }
+    },
+    "NO_CHANGE": { "COUNT": 0, "PERCENTAGE": 0 }
+  },
+  {
+    "STATE": "Assam",
+    "NUMBER_OF_WELLS_ANALYSED": 163,
+    "RISE": {
+      "0-2m": { "COUNT": 84, "PERCENTAGE": 51.5 },
+      "2-4m": { "COUNT": 2, "PERCENTAGE": 1.2 },
+      "4m+": { "COUNT": 1, "PERCENTAGE": 0.6 }
+    },
+    "FALL": {
+      "0-2m": { "COUNT": 72, "PERCENTAGE": 44.2 },
+      "2-4m": { "COUNT": 2, "PERCENTAGE": 1.2 },
+      "4m+": { "COUNT": 2, "PERCENTAGE": 1.2 }
+    },
+    "NO_CHANGE": { "COUNT": 0, "PERCENTAGE": 0 }
+  },
+  {
+    "STATE": "Bihar",
+    "NUMBER_OF_WELLS_ANALYSED": 638,
+    "RISE": {
+      "0-2m": { "COUNT": 291, "PERCENTAGE": 45.6 },
+      "2-4m": { "COUNT": 19, "PERCENTAGE": 3 },
+      "4m+": { "COUNT": 4, "PERCENTAGE": 0.6 }
+    },
+    "FALL": {
+      "0-2m": { "COUNT": 279, "PERCENTAGE": 43.7 },
+      "2-4m": { "COUNT": 36, "PERCENTAGE": 5.6 },
+      "4m+": { "COUNT": 9, "PERCENTAGE": 1.4 }
+    },
+    "NO_CHANGE": { "COUNT": 0, "PERCENTAGE": 0 }
+  }
+];
